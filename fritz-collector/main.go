a0653dfacbf156e1959b005568a66f98f1ab0d58
m0jibake <ae.meerbott@web.de>
2026-02-24 20:51:15 +0100

    fixed bug in variable assignment

--- fritz-collector/main.go
+++ fritz-collector/main.go
@@ -1,73 +1,67 @@
 package main
 
 import (
-	"os"
-	"context"
-	"fmt"
-	"time"
-	"github.com/huin/goupnp/dcps/internetgateway1"
-	"github.com/influxdata/influxdb-client-go/v2" 
+    "os"
+    "context"
+    "fmt"
+    "time"
+    "github.com/huin/goupnp/dcps/internetgateway1"
+    "github.com/influxdata/influxdb-client-go/v2" 
 )
 
 func main() {
+    influx_token := os.Getenv("INFLUX_TOKEN")
+    if influx_token == "" {
+        fmt.Println("Error: No INFLUX_TOKEN env variable has been set.")
+        return
+    }
 
-	influx_token := os.Getenv("INFLUX_TOKEN")
+    clientDB := influxdb2.NewClient("http://192.168.178.49:8086", influx_token)
+    writeAPI := clientDB.WriteAPIBlocking("justme", "mbit-monitoring")
 
-		if influx_token == "" {
-			fmt.Println("Error: No INFLUX_TOKEN env variable has been set.")
-			return
-		}
+    clients, _, err := internetgateway1.NewWANCommonInterfaceConfig1Clients()
+    if err != nil {
+        fmt.Println("Error while searching for Fritz!Box:", err)
+        return
+    }
 
+    if len(clients) > 0 {
+        client := clients[0]
+        fmt.Println("Found a Fritz Box! Starting monitoring...")
 
-	clientDB := influxdb2.NewClient("http://192.168.178.49:8086", influx_token)
-	writeAPI := clientDB.WriteAPIBlocking("justme", "mbit-monitoring")
+        lastBytesSent, _ := client.GetTotalBytesSent()
+        lastBytesReceived, _ := client.GetTotalBytesReceived()
 
-	// 1. Suche nach dem Service "WANCommonInterfaceConfig1"
-	clients, errors, err := internetgateway1.NewWANCommonInterfaceConfig1Clients()
-	
-	if err != nil {
-		fmt.Println("Error while reading data:", err)
-    	return
-	}
+        for {
+            time.Sleep(time.Second * 5)
+            currentBytesSent, _ := client.GetTotalBytesSent()
+            currentBytesReceived, _ := client.GetTotalBytesReceived()
+            
+            if currentBytesSent >= lastBytesSent && currentBytesReceived >= lastBytesReceived {
+                diffSent := currentBytesSent - lastBytesSent
+                diffReceived := currentBytesReceived - lastBytesReceived
 
-	if len(clients) > 0 {
-		client := clients[0] // Wir nehmen die erste FRITZ!Box, die wir finden
-		fmt.Println("Found a Fritz Box!")
+                mbitSent := (float64(diffSent) * 8) / (1000000 * 5)
+                mbitReceived := (float64(diffReceived) * 8) / (1000000 * 5)
 
-		lastBytesSent, _ := client.GetTotalBytesReceived()
-		lastBytesReceived, _ := client.GetTotalBytesSent()
+                fmt.Printf("Download: %.2f Mbit/s | Upload: %.2f Mbit/s\n", mbitReceived, mbitSent)
 
-		time.Sleep(time.Second * 1)
-		for {
-			time.Sleep(time.Second * 5)
-			currentBytesSent, _ := client.GetTotalBytesSent()
-			currentBytesReceived, _ := client.GetTotalBytesReceived()
-			
-			diffSent := currentBytesSent - lastBytesSent
-			diffReceived := currentBytesReceived - lastBytesReceived
+                p := influxdb2.NewPoint("stat", 
+                    map[string]string{"source": "fritzbox"},
+                    map[string]interface{}{"upload": mbitSent, "download": mbitReceived},
+                    time.Now())
 
-			mbitSent := (float64(diffSent) * 8) / (1000000 * 5)
-			mbitReceived := (float64(diffReceived) * 8) / (1000000 * 5)
-
-			fmt.Printf("Current Download Speed: %.2f Mbit/s\n", mbitSent)
-			fmt.Printf("Current Upload Speed: %.2f Mbit/s\n", mbitReceived)
-
-			p := influxdb2.NewPoint("stat", 
-			map[string]string{"source": "fritzbox"},
-			  map[string]interface{}{"sent": mbitSent, "received": mbitReceived},
-  			time.Now())
-
-			err = writeAPI.WritePoint(context.Background(), p)
-			if err != nil {
-				fmt.Printf("InfluxDB Error: %v\n", err)
-			}
-			
-			lastBytesSent = currentBytesSent
-			lastBytesReceived = currentBytesReceived
-		}
-
-    
-	} else {
-		fmt.Println("Keine FRITZ!Box gefunden. Fehler:", errors)
-	}
+                if err := writeAPI.WritePoint(context.Background(), p); err != nil {
+                    fmt.Printf("InfluxDB Error: %v\n", err)
+                }
+            } else {
+                fmt.Println("Counter reset detected (Router rebooted or overflow). Skipping calculation...")
+            }
+            
+            lastBytesSent = currentBytesSent
+            lastBytesReceived = currentBytesReceived
+        }
+    } else {
+        fmt.Println("No Fritz!Box found.")
+    }
 }
